fee54945d98e8ac2e7b6ebb558c1a12c20fb2d5b
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-01-15 20:32:29 +0700

    Initial Commit

---
+++ ContentViewUI/ContentViewUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewUIApp.swift
+//  ContentViewUI
+//
+//  Created by Ярослав Кочкин on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContentViewUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
